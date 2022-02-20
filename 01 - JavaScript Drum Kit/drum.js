2e5ab428043ea5f8eac0d1f07ee23a895a96bd21
Pablo Rosa <panicoro770@gmail.com>
2022-02-20 18:15:51 -0300

    Adding first challenge complete

--- 01 - JavaScript Drum Kit/drum.js
+++ 01 - JavaScript Drum Kit/drum.js
@@ -1,2 +1,15 @@
-console.log('WORK')
-const p     = asd;
+function handleKeyDown(event) {
+  const { keyCode } = event;
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const pressedKey = document.querySelector(`div[data-key="${keyCode}"]`);
+  if (audio) {
+    pressedKey.addEventListener('transitionend', (e) =>
+      e.target.classList.remove('playing')
+    );
+    pressedKey.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
+
+window.addEventListener('keydown', handleKeyDown);
